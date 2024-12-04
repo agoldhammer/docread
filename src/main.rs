89b5bc6f5cc97e671fd83f7657a9c47313b2bab3
Art Goldhammer <art.goldhammer@gmail.com>
2024-12-04 12:37:42 -0500

    returning SearchResult vector

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,17 @@ use docx_rs::*;
 use glob::glob;
 use regex::Regex;
 use serde_json::Value;
-// use std::cell::RefCell;
 use std::collections::VecDeque;
 use std::io::Read;
 use std::path::Path;
+use std::path::PathBuf;
 
 type Run = String;
 type Runs = Vec<Run>;
+struct SearchResult {
+    file_name: String,
+    maybe_result: anyhow::Result<Runs>,
+}
 
 // modified from https://betterprogramming.pub/how-to-parse-microsoft-word-documents-docx-in-rust-d62a4f56ba94
 
@@ -20,13 +24,13 @@ struct Args {
     regex: String,
 }
 
-fn parse_docx(file_name: &Path, search_re: &Regex) -> anyhow::Result<()> {
+fn parse_docx(file_name: &Path, search_re: &Regex) -> anyhow::Result<Runs> {
     let data: Value = serde_json::from_str(&read_docx(&read_to_vec(file_name)?)?.json())?;
     let matched_runs = xtract_text_from_doctree(&data, search_re);
-    for (index, run) in matched_runs.iter().enumerate() {
-        println!("Match: {}-> {}\n", index + 1, run);
-    }
-    Ok(())
+    // for (index, run) in matched_runs.iter().enumerate() {
+    //     println!("Match: {}-> {}\n", index + 1, run);
+    // }
+    Ok(matched_runs)
 }
 
 fn xtract_text_from_doctree(root: &Value, search_re: &Regex) -> Runs {
@@ -60,6 +64,27 @@ fn read_to_vec(path: &Path) -> anyhow::Result<Vec<u8>> {
     Ok(buf)
 }
 
+fn process_files(files: Vec<PathBuf>, search_re: &Regex) -> Vec<SearchResult> {
+    let mut results = Vec::<SearchResult>::new();
+    files.iter().for_each(|file| {
+        // println!("\n*Parsing--> {}\n===", file.display());
+        let result = parse_docx(file.as_path(), &search_re);
+        let search_result = SearchResult {
+            file_name: file.display().to_string(),
+            maybe_result: result,
+        };
+        results.push(search_result);
+    });
+    // match result {
+    //     Ok(runs) => results.push(runs),
+    //     Err(e) => eprintln!("{:?}", e),
+    // };
+    // results.iter().for_each(|runs| {
+    //     println!("\n\n===");
+    //     runs.iter().for_each(|run| println!("{:?}", run));
+    // })
+    results
+}
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     println!("regex: {:#?}\n\n", args.regex);
@@ -75,13 +100,19 @@ fn main() -> anyhow::Result<()> {
         }
     }
     println!("Searching {} files", files.len());
-    // extract matching text from each file
-    for file in files {
-        println!("\n*Parsing--> {}\n===", file.display());
-        match parse_docx(file.as_path(), &re) {
-            Ok(()) => {}
+    let search_results = process_files(files, &re);
+
+    for result in search_results {
+        println!("=== Searched--> {}\n", result.file_name);
+        match result.maybe_result {
+            Ok(runs) => {
+                for (index, run) in runs.iter().enumerate() {
+                    println!("Match: {}-> {}\n", index + 1, run);
+                }
+            }
             Err(e) => eprintln!("{:?}", e),
-        };
+        }
     }
+
     Ok(())
 }
